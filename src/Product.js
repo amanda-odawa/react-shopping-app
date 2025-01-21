dbcd915c45fb27fdce052f20240afabe6cc4849f
AmandaOdawa <amandaodawa1@gmail.com>
2025-01-21 15:46:59 +0300

    remove images

--- src/Product.js
+++ src/Product.js
@@ -1,12 +1,8 @@
 import React from "react";
 
 const Product = ({ product, addToCart }) => {
-
-  const productImage = `/images/${product.name.toLowerCase()}.jpg`;
-
   return (
     <div className="product">
-      <img src={productImage} alt={product.name} className="product-image" />
       <h3>{product.name}</h3>
       <p>${product.price}</p>
       <button onClick={() => addToCart(product)}>Add to Cart</button>
